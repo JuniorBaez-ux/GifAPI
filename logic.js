4630640b913a4af178faa704bbd9b26ad78af21c
Junior Baez <junior_baez@ucne.edu.do>
2022-05-12 16:35:48 -0400

    Implementing async functionality

--- logic.js
+++ logic.js
@@ -4,21 +4,13 @@ const API_KEY = 'DObRD9EoCneCObFAd57k5S7P9mptjYeE';
 const searchString = 'api_key=' + API_KEY;
 
 //Function to fetch GIFS from the website
-function fetchImage(searchParam) {
-
+async function fetchImage(searchParam) {
     let stringToSearch = document.getElementById('search').value; 
     searchParam = stringToSearch !== "" ? stringToSearch.toLowerCase() : 'dogs';
 
-    fetch('https://api.giphy.com/v1/gifs/translate?' + searchString + '&s=' + searchParam, {mode: 'cors'})
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(response) {
-        img.src = response.data.images.original.url;
-    }).catch(e => {
-        alert("The process could not be completed. Please try again later.");
-        console.log(e)
-});
+    const response = await fetch('https://api.giphy.com/v1/gifs/translate?' + searchString + '&s=' + searchParam, {mode: 'cors'});
+    const dogData = await response.json();
+    img.src = response.data.images.original.url;
 }
 
 document.onload = fetchImage();
